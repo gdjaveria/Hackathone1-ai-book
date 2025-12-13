c265ed03a4b2f71bef22555f6c7ec16d0506be97
gdjaveria <javeriajiapk@gmail.com>
2025-12-13 14:22:52 -0800

    project

--- physical-ai-book/src/pages/index.tsx
+++ physical-ai-book/src/pages/index.tsx
@@ -21,7 +21,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            welcome to AI book ⏱️
           </Link>
         </div>
       </div>
